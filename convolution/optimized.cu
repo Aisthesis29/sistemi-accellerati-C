f922708f6d6c7e4041114ea46cb75dc153c0fea4
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-22 22:30:34 +0100

    calmo

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -105,7 +105,7 @@ __global__ void bilateral_u8_gray(uchar4 *rgba, unsigned char *out, int width, i
              if(idx0==DEBUG_IDX){
            //printf( "sono entrato");
                 printf("down1=%d down=%d",idxDown1,idxDown);
-        }
+                     }
                 uchar4 valUpp = rgba[idxUpp];
                 int val_rU = (int)valUpp.x;
                 int val_gU = (int)valUpp.y;
@@ -141,9 +141,9 @@ __global__ void bilateral_u8_gray(uchar4 *rgba, unsigned char *out, int width, i
                       //printf( "val rgD etc D=%d %d %d",val_rD,val_gD,val_bD);valori ok
 
                 }
-               
+               i++;
             }
-             i++;
+             
               //wsum=0;
              
         }
