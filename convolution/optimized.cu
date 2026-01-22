28c2ed4d19eadc9ea44ffca9a249eb2a8a65d2ea
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-22 21:52:30 +0100

    stanco

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -175,16 +175,15 @@ __global__ void bilateral_u8_gray(uchar4 *rgba, unsigned char *out, int width, i
        
         if(idx0==DEBUG_IDX){
            printf("gPU idx=%d\nin   = R:%d G:%d B:%d\n"
-                  "in*peso   = R:%.9f G:%.9f B:%.9f\ntmp  = R:%d G:%d B:%d\n",
+                  "\ntmp  = R:%d G:%d B:%d\n",
                     idx0,
                     
-                    h_input[idx0*3],
-                    h_input[idx0*3 + 1],
-                    h_input[idx0*3 + 2],
+                    rgba[idx0].x,
+                    rgba[idx0].y,
+                    rgba[idx0].z,
                     out[idx0*3],
                     out[idx0*3+1],
-                    out[idx0*3+2],
-                    tmp1, tmp2, tmp3);
+                    out[idx0*3+2]);
         }
     }
 }
