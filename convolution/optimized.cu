113fa59fc5f5de8096e8087d8bbe2e4f48db627b
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-23 15:31:28 +0100

    double kernel

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -491,8 +491,10 @@ unsigned char *out_inner=d_output+3*(width)*dim_kernel;
 //first row
 //bilateral_u8_gray_unopt<<<grid, block>>>(rgba, out_first, width, 1, radius, d_space_weight, d_color_weight, dim_kernel);
 //dim3 block(blockSize, blockSize);
-dim3 grid_row((width + block.x - 1)/block.x,(dim_kernel + block.y - 1)/block.y);
-bilateral_u8_gray_unopt_ybase<<<grid, block>>>(
+int rows = /* la porzione che vuoi calcolare con questo launch */;
+dim3 grid_row((width + block.x - 1) / block.x,
+              (rows  + block.y - 1) / block.y);
+bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
     rgba, out_first,                 // base pointers (immagine intera)
     width, height,                  // DIMENSIONI REALI
     0, height,                           // y_base=0, rows=1  -> solo prima riga
