e5403e1a67d1fca2ec5395c41cd08e57442a8aff
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-23 13:55:58 +0100

    double kernel

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -490,9 +490,8 @@ bilateral_u8_gray<<<grid, block>>>(first_row_end, out_inner, width, height-2, ra
 //first row
 //bilateral_u8_gray_unopt<<<grid, block>>>(rgba, out_first, width, 1, radius, d_space_weight, d_color_weight, dim_kernel);
 //dim3 block(blockSize, blockSize);
-dim3 grid_row((width + block.x - 1) / block.x,
-              (1 + block.y - 1) / block.y);
-bilateral_u8_gray_unopt_ybase<<<grid, block>>>(
+dim3 grid_row((width + block.x - 1)/block.x,(1 + block.y - 1)/block.y);
+bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
     rgba, d_output,                 // base pointers (immagine intera)
     width, height,                  // DIMENSIONI REALI
     0, 1,                           // y_base=0, rows=1  -> solo prima riga
@@ -503,8 +502,7 @@ bilateral_u8_gray_unopt_ybase<<<grid, block>>>(
 
 //last row
 //bilateral_u8_gray_unopt<<<grid, block>>>(last_row_start, out_last, width, 1, radius, d_space_weight, d_color_weight, dim_kernel);
-dim3 grid_last((width + block.x - 1)/block.x, (1 + block.y - 1)/block.y);
-bilateral_u8_gray_unopt_ybase<<<grid, block>>>(
+bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
     rgba, d_output, width, height,
     height-1, 1,
     radius, d_space_weight, d_color_weight, dim_kernel); 
