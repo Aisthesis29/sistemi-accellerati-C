0fe0892c1dd6e2ab980bc447a52ebff161ad1122
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-23 14:03:10 +0100

    double kernel

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -484,8 +484,10 @@ uchar4 *last_row_start=rgba+(width*height)-(width);
 unsigned char *out_first=d_output;
 unsigned char *out_last=d_output+(3*width*height)-(3*width);
 unsigned char *out_inner=d_output+3*(width);
+unsigned char *out_innerALT=d_output;
+
 //inner
-bilateral_u8_gray<<<grid, block>>>(first_row_end, out_inner, width, height-2, radius, d_space_weight, d_color_weight, dim_kernel);
+bilateral_u8_gray<<<grid, block>>>(first_row_end, out_innerALT, width, height-1, radius, d_space_weight, d_color_weight, dim_kernel);
 
 //first row
 //bilateral_u8_gray_unopt<<<grid, block>>>(rgba, out_first, width, 1, radius, d_space_weight, d_color_weight, dim_kernel);
@@ -502,10 +504,10 @@ bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
 
 //last row
 //bilateral_u8_gray_unopt<<<grid, block>>>(last_row_start, out_last, width, 1, radius, d_space_weight, d_color_weight, dim_kernel);
-bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
+/*bilateral_u8_gray_unopt_ybase<<<grid_row, block>>>(
     rgba, d_output, width, height,
     height-1, 1,
-    radius, d_space_weight, d_color_weight, dim_kernel); 
+    radius, d_space_weight, d_color_weight, dim_kernel); */
 // ========== Salvataggio immagini ==========
     
     CHECK(cudaMemcpy(h_output, d_output, imageSize, cudaMemcpyDeviceToHost));
