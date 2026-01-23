aba4cb6804b5f96831009b70ff001045f36117a5
Aisthesis29 <querze.alessandro@gmail.com>
2026-01-23 13:21:23 +0100

    double kernel

--- convolution/optimized.cu
+++ convolution/optimized.cu
@@ -474,10 +474,10 @@ int main(int argc, char **argv) {
     
 //######################second try
 uchar4 *first_row_end=rgba+width;//non serve fare *3 perchè è uchar4
-uchar4 *last_row_start=rgba+(width*(height-1))-(width);
+uchar4 *last_row_start=rgba+(width*height)-(width);
 
 unsigned char *out_first=d_output;
-unsigned char *out_last=d_output+(3*width*(height-1))-(3*width);
+unsigned char *out_last=d_output+(3*width*height)-(3*width);
 unsigned char *out_inner=d_output+3*(width);
 //inner
 bilateral_u8_gray<<<grid, block>>>(first_row_end, out_inner, width, height-2, radius, d_space_weight, d_color_weight, dim_kernel);
