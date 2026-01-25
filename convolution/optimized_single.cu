805f7854c82d01685ad7996643343019274f44be
MirkoTonelli <mirkojudo02@gmail.com>
2026-01-25 17:24:08 +0100

    p

--- convolution/optimized_single.cu
+++ convolution/optimized_single.cu
@@ -176,7 +176,7 @@ __global__ void bilateral_u8_gray(uchar4 *rgba, unsigned char *out, int width, i
     } else {
         if(x<10)
             printf("rows: %d\n", rows);
-        int y = rows + y_local;               // y globale
+        int y = rows + y_local + radius;               // y globale
         idx0 = y * width +x;
         uchar4 pixel = rgba[idx0];
         int center_r = (int)pixel.x;
