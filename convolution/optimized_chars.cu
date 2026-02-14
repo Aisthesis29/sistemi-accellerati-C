1f90a220909fc382fee076da79fdfdfae2a05e9e
Aisthesis29 <querze.alessandro@gmail.com>
2026-02-14 20:30:16 +0100

    gridblocks

--- convolution/optimized_chars.cu
+++ convolution/optimized_chars.cu
@@ -293,6 +293,8 @@ int main(int argc, char **argv) {
     int  sigma_r = atoi(argv[4]);
     int block_x = atoi(argv[5]);
     int block_y = atoi(argv[6]);
+    int grid_x = atoi(argv[7]);
+    int grid_y = atoi(argv[8]);
     if(dim_kernel%2==0 || dim_kernel<1) {
         printf("kernel_size: valore non valido. Deve essere un intero positivo dispari\n");
         return 2;
@@ -333,7 +335,7 @@ int main(int argc, char **argv) {
     CHECK(cudaMemcpy(d_input, h_input,  imageSize, cudaMemcpyHostToDevice)); //match anche qui
 
     dim3 block(block_x, block_y);
-    dim3 grid((width + block.x - 1) / block.x, (height + block.y - 1) / block.y);
+    dim3 grid(grid_x,grid_y);
     // ========== Operazioni reali ==========
     memory_alignment<<<grid, block>>>(d_input, rgba, width, height);
     int ds2;
